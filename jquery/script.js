fe3a0a24954ba4db0944bc84025e79e4459cc7dc
yassine ait ouaddi <98220932+aitouadd@users.noreply.github.com>
2022-10-08 17:28:40 +0100

    Add files via upload

---
+++ jquery/script.js
@@ -0,0 +1,41 @@
+$(".todoinput").keyup(function(mouseenter) {
+	if(mouseenter.which === 13) {
+		if($(this).val()==="") return;
+		$("ul").prepend("<li><span class='left'><i class='fa fa-trash'></i></span>  "+$(this).val()+"<span class='right'><i class='fa fa-pencil'></span></li>");
+		$(this).val("");
+	}
+});
+
+$(".fa-plus").on("click", function() {
+	$("input").slideToggle();
+});
+
+$("ul").on("click", "span.text", function(event) {
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span.left", function(event) {
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("ul").on("click", "span.right", function(event) {
+	var parent = $(this).parent();
+	var oldVal = parent.text();
+	parent.html("<input type='text' class='editinput'>");
+	$(".editinput").keypress(function(e) {
+		if(e.which === 13) {
+			if($(this).val() === "") {
+				parent.html("<span class='left'><i class='fa fa-trash'></i></span><span class='text'>  "+oldVal+"</span><span class='right'><i class='fa fa-pencil'>");	
+			}
+			else {
+				var newVal = $(this).val();
+				parent.html("<span class='left'><i class='fa fa-trash'></i></span><span class='text'>  "+newVal+"</span><span class='right'><i class='fa fa-pencil'>");
+			}
+		}
+		e.stopPropagation();
+	});
+	event.stopPropagation();
+});
